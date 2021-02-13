9d7c901243a2fc271acf908bb96f4aef1e1a5ff4
niteshbhardwaj960 <nitesh.bhardwaj@yahoo.com>
2021-02-13 12:05:01 +0530

    New Edit Page and Menu Modification

--- WebERP/Controllers/ArticalController.cs
+++ WebERP/Controllers/ArticalController.cs
@@ -33,8 +33,9 @@ namespace WebERP.Controllers
             return View(dbContext.Artical_Master.ToList());
         }
         [HttpGet]
-        public IActionResult AddArtical()
+        public IActionResult AddArtical(int ID, string Type)
         {
+
             return View();
         }
         [HttpPost]
