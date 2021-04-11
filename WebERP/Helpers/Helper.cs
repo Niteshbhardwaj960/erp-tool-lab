905a3e62e800dbb6739a12844399f1ca9fafea01
ashwin-lab <k93.ashwini@gmail.com>
2021-04-11 22:48:34 +0530

    Further chnages in PO JW pages

---
+++ WebERP/Helpers/Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebERP.Helpers
+{
+    public static class Helper
+    {
+        public static int GetFinYear()
+        {
+            string FinYear = "";
+            DateTime date = DateTime.Now;
+            if ((date.Month) == 1 || (date.Month) == 2 || (date.Month) == 3)
+            {
+                FinYear = (date.Year - 1) + "" + date.Year;
+            }
+            else
+            {
+                FinYear = date.Year + "" + (date.Year + 1);
+            }
+            return Convert.ToInt32(FinYear);
+        }
+    }
+}
--- WebERP/wwwroot/scripts/erpJobWork.js
+++ WebERP/wwwroot/scripts/erpJobWork.js
@@ -1,18 +1,68 @@
-﻿
-$("#addProcess").click(function (e) {
-    debugger
-    e.preventDefault();
-    var tempProc = $('.jwProcDropDown').find('option:selected').val();
-    var gridProc = tempProc; //.substr(6, 1);   
-    $('#jwDtlTable').find('tr').each(function () {
-        debugger
-        var row = $(this);
-        if (row.find('input[type="checkbox"]').is(':checked')) {
-            row.find('[name*="PROC_CODE"]').val(gridProc);
-            row.find('input[type="checkbox"]').prop("checked", false);
-        }
+﻿$(document).ready(function () {
+    // Setup - add a text input to each footer cell
+    $('#jwDtlTable thead tr').clone(true).appendTo('#jwDtlTable thead');
+    $('#jwDtlTable thead tr:eq(1) th').each(function (i) {
+        var title = $(this).text();
+        $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+
+        $('input', this).on('keyup change', function () {
+            if (table.column(i).search() !== this.value) {
+                table
+                    .column(i)
+                    .search(this.value)
+                    .draw();
+            }
+        });
+    });
+
+    var table = $('#jwDtlTable').DataTable({
+        orderCellsTop: true,
+        fixedHeader: true
     });
 });
+
+$('#jwComDropDown').change(function (e) {
+    e.preventDefault();
+    var tempProc = $('#jwComDropDown').find('option:selected').val();
+    var gridProc = tempProc; 
+    $('#jwDtlTable').find('tr').each(function () {        
+        var row = $(this);
+        var qtyFilled = row.find("td:eq(7) input[type='number']").val();
+        if (qtyFilled == null || typeof qtyFilled === "undefined" || qtyFilled=="") {
+            currentRow.find('[name*="PROC_CODE"]').val("0");
+        }
+        else {
+            row.find('[name*="PROC_CODE"]').val(gridProc);
+        }        
+    });
+});
+
+$('.jwUserFilledQty').change(function (e) {    
+    e.preventDefault();
+    var currentRow = $(this).closest('tr');   
+    var qtyFilled = currentRow.find("td:eq(7) input[type='number']").val();
+    if (qtyFilled == null || typeof qtyFilled === "undefined" || qtyFilled == "") {        
+        currentRow.find('[name*="PROC_CODE"]').val("0");
+    }
+    else {
+        var gridProc = $('#jwComDropDown').find('option:selected').val();
+        currentRow.find('[name*="PROC_CODE"]').val(gridProc);
+    }  
+});
+
+//$("#addProcess").click(function (e) {    
+//    e.preventDefault();
+//    var tempProc = $('.jwProcDropDown').find('option:selected').val();
+//    var gridProc = tempProc; //.substr(6, 1);   
+//    $('#jwDtlTable').find('tr').each(function () {
+//        debugger
+//        var row = $(this);
+//        if (row.find('input[type="checkbox"]').is(':checked')) {
+//            row.find('[name*="PROC_CODE"]').val(gridProc);
+//            row.find('input[type="checkbox"]').prop("checked", false);
+//        }
+//    });
+//});
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
